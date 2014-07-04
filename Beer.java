8595fd9b88162089beb00043ed20179109f0c622
Phạm Thế Nam <nampt_57@vnu.edu.vn>
2014-07-04 14:26:40 +0700

    Bài tập lớn 01

---
+++ Beer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package BaiTap;
+
+/**
+ *
+ * @author Nam
+ */
+public class Beer {
+    public static void main(String [] args) {
+        System.out.println("Ho va ten: Pham The Nam \t Ma sinh vien: 12020262 \t Lop: K57CB \t Username Github: Nampt \t Email: nampt_57@vnu.edu.vn \t");
+        int i;
+        for(i=9; i>=1; i--){
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.\nTake one down, pass it aroud,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
