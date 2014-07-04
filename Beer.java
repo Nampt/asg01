48f82ecab6755717b98c781f0ded1f78084ad72e
Phạm Thế Nam <nampt_57@vnu.edu.vn>
2014-07-04 21:49:26 +0700

    Update Beer.java

--- Beer.java
+++ Beer.java
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package BaiTap;
-
-/**
- *
- * @author Nam
- */
+//Bai tap lon 01
 public class Beer {
     public static void main(String [] args) {
         System.out.println("Ho va ten: Pham The Nam \t Ma sinh vien: 12020262 \t Lop: K57CB \t Username Github: Nampt \t Email: nampt_57@vnu.edu.vn \t");
